9779193dc94d413d0ff3287cea53f3ded609598a
AjaykumrReddy <reddyajaykumar430@gmail.com>
2021-06-05 09:19:42 +0530

    Update settings.py

--- donorsite/settings.py
+++ donorsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lo63q3f4v#4lqx+hc+2d=ss!pf74f_#%8m=*o=cnmsd)f92he#'
+SECRET_KEY = '#############################################'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
